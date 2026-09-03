b1963d8b5c21b85b76bd1031f39e46dd338ff780
BilalMemon07 <bilalsaylani451@gmail.com>
2026-09-03 17:23:21 +0500

    update

--- custom_invoice_format/__manifest__.py
+++ custom_invoice_format/__manifest__.py
@@ -1,9 +1,9 @@
 {
     'name': 'Custom Invoice Format (Official/Unofficial)',
-    'version': '18.0.0.1.0',
+    'version': '18.0.0.1.1',
     'category': 'Accounting/Accounting',
     'summary': 'Company-specific custom invoice fields and invoice layout',
-    'depends': ['account', 'product'],
+    'depends': ['account', 'product', 'hoa_fmcg_brand_reporting'],
     'data': [
         'views/res_company.xml',
         'views/partner_view.xml',
